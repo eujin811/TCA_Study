5103d00ecb86bee3c79e90b4fdecea52a955eeed
eujin811 <eujin8@gmail.com>
2022-11-07 17:30:15 +0900

    :tada:

---
+++ TCA_Notice_ex/TCA_Notice_ex/TCA_Notice_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Notice_exApp.swift
+//  TCA_Notice_ex
+//
+//  Created by YoujinMac on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Notice_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_Notice_ex/TCA_Notice_exUITests/TCA_Notice_exUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TCA_Notice_exUITestsLaunchTests.swift
+//  TCA_Notice_exUITests
+//
+//  Created by YoujinMac on 2022/11/07.
+//
+
+import XCTest
+
+class TCA_Notice_exUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
