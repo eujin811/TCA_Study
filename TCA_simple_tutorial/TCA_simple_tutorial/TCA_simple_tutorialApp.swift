6ca1d9301089c6a814b1cc8edad7c872e789ab3f
eujin811 <eujin8@gmail.com>
2022-11-07 18:19:06 +0900

    :construction: tca tutorial

---
+++ TCA_simple_tutorial/TCA_simple_tutorial/TCA_simple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_simple_tutorialApp.swift
+//  TCA_simple_tutorial
+//
+//  Created by YoujinMac on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct TCA_simple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
