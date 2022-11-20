6e73cb7f284c8c742ed425a27c3db58a0d33dbf5
eujin811 <eujin8@gmail.com>
2022-11-20 12:59:22 +0900

    :construction: Constants

---
+++ TCA_Notice_ex/TCA_Notice_ex/Common/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TCA_Notice_ex
+//
+//  Created by YoujinMac on 2022/11/20.
+//
+
+import Foundation
+
+enum Constants {
+    static let sever = SeverConstants.self
+    
+}
